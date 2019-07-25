e84bb2684b99579f053d48198ff65864b8b39a9f
Misaki SATA <misaki.sata@gmail.com>
2019-07-26 01:32:04 +0900

    set localhost

--- src/xv_rugby/settings.py
+++ src/xv_rugby/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'xv_rugby',
         'USER': 'xv_rugby',
         'PASSWORD': 'xv_rugby',
-        'HOST': 'mysql',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
