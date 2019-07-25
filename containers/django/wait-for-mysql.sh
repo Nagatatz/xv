db617428af1325ef844747d790c444e0cd5d958d
Misaki SATA <misaki.sata@gmail.com>
2019-07-26 03:41:24 +0900

    entrypoint move to Dockerfile

--- containers/django/wait-for-mysql.sh
+++ containers/django/wait-for-mysql.sh
@@ -5,13 +5,15 @@ set -e
 
 host="$1"
 shift
+port="$1"
+shift
 user="$1"
 shift
 password="$1"
 shift
 
 echo "Waiting for mysql"
-until mysql -h"$host" -u"$user" -p"$password" &> /dev/null
+until mysql -h"$host" -P"$port" -u"$user" -p"$password" &> /dev/null
 do
         >$2 echo -n "."
         sleep 1
