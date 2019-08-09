f747f1e83a879df6d83a4781dcac7c3fb5f3346e
Misaki SATA <misaki.sata@gmail.com>
2019-08-10 02:50:09 +0900

    fix member profile relation

--- src/member/models.py
+++ src/member/models.py
@@ -39,20 +39,6 @@ class Team(BaseModel):
         db_table = 'rugby_team'
 
 
-class PlayerProfile(BaseModel):
-    """
-    選手プロフィール
-    """
-
-    height = models.DecimalField(verbose_name='身長', max_digits=4, decimal_places=1)
-    weight = models.DecimalField(verbose_name='体重', max_digits=4, decimal_places=1)
-    player_position = models.ManyToManyField(PlayerPosition, 'ポジション')
-
-    class Meta:
-        verbose_name_plural = '選手プロフィール'
-        db_table = 'rugby_player_profile'
-
-
 class Member(BaseModel):
     """
     チーム人員
@@ -64,9 +50,6 @@ class Member(BaseModel):
     team_group = models.ForeignKey(
         TeamGroup, on_delete=models.SET_NULL, null=True, verbose_name='所属チームグループ'
     )
-    player_profile = models.OneToOneField(
-        PlayerProfile, on_delete=models.CASCADE, null=True, verbose_name='選手プロフィール'
-    )
     staff_position = models.ManyToManyField(StaffPosition, verbose_name='役職')
 
     def __str__(self):
@@ -75,3 +58,20 @@ class Member(BaseModel):
     class Meta:
         verbose_name_plural = 'チーム人員'
         db_table = 'rugby_member'
+
+
+class PlayerProfile(BaseModel):
+    """
+    選手プロフィール
+    """
+
+    member = models.ForeignKey(
+        Member, null=True, unique=True, on_delete=models.CASCADE, verbose_name='人員'
+    )
+    height = models.DecimalField(verbose_name='身長', max_digits=4, decimal_places=1)
+    weight = models.DecimalField(verbose_name='体重', max_digits=4, decimal_places=1)
+    player_position = models.ManyToManyField(PlayerPosition, verbose_name='ポジション')
+
+    class Meta:
+        verbose_name_plural = '選手プロフィール'
+        db_table = 'rugby_player_profile'
